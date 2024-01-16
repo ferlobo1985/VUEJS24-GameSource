d498a312f0d0474d47e3ac78e7dc1ba37af1bdf0
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-01-16 16:38:46 -0300

    02-Installing libraries
    02-Installing libraries

--- src/main.js
+++ src/main.js
@@ -6,9 +6,28 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+
+/// TOASTS
+import ToastPlugin from 'vue-toast-notification';
+import 'vue-toast-notification/dist/theme-bootstrap.css';
+
+/// VUETIFY
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css' 
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
+app.use(ToastPlugin);
 
 app.mount('#app')
