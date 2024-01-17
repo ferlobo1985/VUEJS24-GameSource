ccbc7557dc07b6c784c957de03177b1cc0fc3be7
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-01-17 18:04:15 -0300

    25-Load more Admin articles
    25-Load more Admin articles

--- src/components/user/dashboard/admin/articles.vue
+++ src/components/user/dashboard/admin/articles.vue
@@ -51,8 +51,20 @@
                 </tr>
             </tbody>
         </v-table>
-    </div>
 
+        <div class="text-center m-3" v-if="btnLoad">
+            <v-progress-circular indeterminate color="primary"/>
+        </div>
+        <v-btn
+            v-else
+            variant="outlined"
+            class="mt-3"
+            @click="loadMoreHandler()"
+        >
+            Get more articles
+        </v-btn>
+
+    </div>
 </template>
 
 <script setup>
@@ -70,6 +82,13 @@
 
 
     /// LOAD MORE ARTICLES
+    const loadMoreHandler = () => {
+        btnLoad.value = true;
+        articleStore.adminGetMoreArticles(3)
+        .finally(()=>{
+            btnLoad.value = false
+        })
+    }
 
     //// GET FIRST ARTICLES
     if(!articleStore.adminArticles || route.query.reload){
--- src/stores/articles.js
+++ src/stores/articles.js
@@ -69,6 +69,34 @@ export const useArticleStore = defineStore('article',{
                 throw new Error(error)
             }
         },
+        async adminGetMoreArticles(docLimit){
+            try{
+                if(this.adminLastVisible){
+                    let oldArticles = this.adminArticles;
+
+                    const q = query(
+                        articlesCol,
+                        orderBy('timestamp','desc'),
+                        startAfter(this.adminLastVisible),
+                        limit(docLimit)
+                    );
+                    const querySnapshot = await getDocs(q);
+                    const lastVisible = querySnapshot.docs[querySnapshot.docs.length-1];
+                    const newArticles = querySnapshot.docs.map(doc=>({
+                        id:doc.id,
+                        ...doc.data()
+                    }));
+
+                    this.adminArticles = [
+                        ...oldArticles,
+                        ...newArticles
+                    ];
+                    this.adminLastVisible = lastVisible;
+                }
+            }catch(error){
+                throw new Error(error)
+            }
+        },
         async adminGetArticles(docLimit){
             try{
                 const q = query(articlesCol,orderBy('timestamp','desc'),limit(docLimit));
